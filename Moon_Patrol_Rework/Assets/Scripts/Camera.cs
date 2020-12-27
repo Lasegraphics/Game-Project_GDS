cbd660a8b9db6a6e4184ac5a8f9fdaeedb6be674
Kacperb96 <69037818+Kacperb96@users.noreply.github.com>
2020-12-27 21:14:16 +0100

    ds
    ds

---
+++ Moon_Patrol_Rework/Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject car;
+    public float smooth; 
+    public Vector3 currVelocity;
+
+    void Update()
+    {
+        Vector3 newCamPosition = new Vector3 (car.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, newCamPosition, ref currVelocity, smooth);
+    }
+}
---
+++ Moon_Patrol_Rework/Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    public float horizontalBulletSpeed;
+    [SerializeField] GameObject VerticalBulletPrefab;
+    public float VerticalBulletSpeed;
+    public float HorizontalTime;
+    public float VerticalTime;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootHorizontalBullet();
+            ShootVerticalBullet();    
+        }
+
+    }
+
+    void ShootHorizontalBullet()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab);
+        bullet.transform.position = new Vector3 (transform.position.x, transform.position.y + .3f, transform.position.z);
+        bullet.transform.SetParent(null);
+        bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalBulletSpeed, 0f), ForceMode2D.Force);
+        Destroy(bullet, HorizontalTime);
+    }
+
+    void ShootVerticalBullet()
+    {
+        GameObject VerticalBullet = Instantiate(VerticalBulletPrefab, gameObject.transform);
+        VerticalBullet.transform.SetParent(null);
+        VerticalBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(170f, VerticalBulletSpeed), ForceMode2D.Force);
+        Destroy(VerticalBullet, VerticalTime);
+    }
+}
