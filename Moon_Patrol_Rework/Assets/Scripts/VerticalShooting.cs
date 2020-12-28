a4d053bd063177e199d1753fab2d6d611200601a
Kacperb96 <69037818+Kacperb96@users.noreply.github.com>
2020-12-29 00:56:03 +0100

    strzelanie
    strzelanie

---
+++ Moon_Patrol_Rework/Assets/Scripts/VerticalShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalShooting : MonoBehaviour
+{
+    [SerializeField] GameObject VerticalBulletPrefab;
+    public float VerticalBulletSpeed;
+    public float VerticalTime;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootVerticalBullet();    
+        }
+    }
+
+    void ShootVerticalBullet()
+    {
+        GameObject VerticalBullet = Instantiate(VerticalBulletPrefab, gameObject.transform);
+        VerticalBullet.transform.SetParent(null);
+        VerticalBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(170f, VerticalBulletSpeed), ForceMode2D.Force);
+        Destroy(VerticalBullet, VerticalTime);
+    }
+}
