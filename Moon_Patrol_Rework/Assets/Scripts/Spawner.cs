bebb05fd78a4454d668904020fc853d8df181b5f
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2020-12-28 13:49:11 +0100

    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject objectToSpawn; 
+    SpriteRenderer spriteR; 
+    
+    void Start()
+    {
+        
+        spriteR = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter2D (Collider2D other)
+    {
+
+        
+        Instantiate(objectToSpawn, transform.position, transform.rotation);
+
+        spriteR.isVisible();
+
+    }
+
+
+}
