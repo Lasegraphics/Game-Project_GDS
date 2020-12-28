9139e907c9bc54555081531d5edf16d77bf3fb7e
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2020-12-28 13:28:10 +0100

    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/Pick_Up.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick_Up : MonoBehaviour
+{
+
+    Counter counter;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = GameObject.Find("Manager").GetComponent<Counter>();
+        
+        if (counter == null)
+        {
+
+            Debug.LogError("Cannot find Counter");
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        
+        
+            Destroy (this.gameObject);
+
+            counter.IncrementCounter();
+
+
+        
+
+
+    }
+}
