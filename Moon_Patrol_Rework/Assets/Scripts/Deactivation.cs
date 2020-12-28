95aeb62beab4c0af2f8e8eab87226a694d41fd1e
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2020-12-28 17:43:59 +0100

    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/Deactivation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivation : MonoBehaviour
+{
+    private bool onTheGround;
+
+    public LayerMask layersToTest;
+    public Transform groundTester;
+
+    private float radius = 0.000001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    onTheGround = Physics2D.OverlapCircle(groundTester.position, radius, layersToTest);
+
+    if (onTheGround)
+    {
+
+        Destroy(this.gameObject);
+
+    }
+
+    }
+}
