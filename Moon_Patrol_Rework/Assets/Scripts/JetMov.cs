052c8a1c67115803a1c7af4e7b7e17fd0034e0be
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2020-12-29 13:12:42 +0100

    sd
    sd

---
+++ Moon_Patrol_Rework/Assets/Scripts/JetMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetMov : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public float speed; 
+
+    void Start()
+    {
+        
+        
+        transform.localScale = new Vector2(1,1);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(2* Time.deltaTime * speed, 0,0);
+
+
+    }
+}
