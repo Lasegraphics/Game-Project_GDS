080f474f7652eb827509c9f5b4e40c50f456d470
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2020-12-29 15:44:13 +0100

    sd
    sd

--- Moon_Patrol_Rework/Assets/Scripts/JetMov.cs
+++ Moon_Patrol_Rework/Assets/Scripts/JetMov.cs
@@ -11,31 +11,48 @@ public class JetMov : MonoBehaviour
 
     public Rigidbody2D rb;
 
+
+    public JetPingPong en; 
+  
+    
+
     void Start()
     {
+
+        en = GetComponent<JetPingPong>();
         
-        
+        en.enabled = false;
+
+
         transform.localScale = new Vector2(1,1);
 
         Invoke("StartMov",2f);
 
         Invoke("EndStartMov",3f);
+        Invoke ("EnablingScript", 3f);
+
+        Invoke ("DisablingScript",10f);
+
+        Invoke("StartMov",10f);
+        
 
 
     }
 
+
+
+
+
     void StartMov()
 
     {
-
-
-
+           
 
                 rb.AddForce(new Vector2(speed,0),ForceMode2D.Impulse);
 
                  //transform.Translate(2* Time.deltaTime * speed, 0,0);
 
-          
+           
     
     }
 
@@ -46,6 +63,23 @@ public class JetMov : MonoBehaviour
          rb.velocity = Vector2.zero;
 
     }
+
+    void EnablingScript ()
+    {
+
+
+        en.enabled = true; 
+
+    }
+
+    void DisablingScript()
+    {
+
+        en.enabled = false;
+
+
+    }
+  
 
 
 
