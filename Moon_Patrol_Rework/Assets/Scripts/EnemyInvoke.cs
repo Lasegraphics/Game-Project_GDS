ebf2f445d87fa5f55ebdaabc5b11a55eaf5aa431
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-04 20:08:31 +0100

    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/EnemyInvoke.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInvoke : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+     public GameObject Enemy1;
+     public GameObject Enemy2;
+    public GameObject Enemy3;
+
+
+    public GameObject Enemy4;
+
+    void Start()
+
+    {
+
+
+        Enemy1.SetActive(false);
+        Enemy2.SetActive(false);
+        Enemy3.SetActive(false);
+        Enemy4.SetActive(false);
+       Invoke("EnablingEnemy1", 1f);
+        Invoke("EnablingEnemy2", 2f);
+        Invoke("EnablingEnemy3", 10f);
+        Invoke("EnablingEnemy4", 10f);
+    }
+
+    // Update is called once per frame
+   
+
+    public void EnablingEnemy1() => Enemy1.SetActive(true);
+    public void EnablingEnemy2() => Enemy2.SetActive(true);
+    public void EnablingEnemy3() => Enemy3.SetActive(true);
+    public void EnablingEnemy4() => Enemy4.SetActive(true);
+   
+
+
+
+
+
+}
---
+++ Moon_Patrol_Rework/Assets/Scripts/ReleaseEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    EnemyInvoke trigger;
+
+    void Start()
+    {
+
+
+
+        
+    }
+
+    // Update is called once per frame
+   
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+      if(other.gameObject.tag == "Body")
+      {
+
+      trigger.EnablingEnemy1();
+      trigger.EnablingEnemy2();
+      trigger.EnablingEnemy3();
+      trigger.EnablingEnemy4();
+
+
+      }
+
+  }
+
+}
