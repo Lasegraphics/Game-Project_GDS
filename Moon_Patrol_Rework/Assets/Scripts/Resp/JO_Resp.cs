3a53c2f1ace65cff1196ecd9b9408c715764181d
Kacperb96 <69037818+Kacperb96@users.noreply.github.com>
2021-01-13 11:42:48 +0100

    Update Scen

---
+++ Moon_Patrol_Rework/Assets/Scripts/Resp/JO_Resp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JO_Resp : MonoBehaviour
+{
+    void OnCollisionEnter2D()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
