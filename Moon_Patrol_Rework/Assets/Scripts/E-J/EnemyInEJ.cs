df198789ef62f8217087368c69c0d7a9addc46e2
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-13 18:00:40 +0100

    s
    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/E-J/EnemyInEJ.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInEJ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    public GameObject Enemy4;
+    public GameObject Enemy5;
+    public GameObject Enemy6;
+    public GameObject Enemy7;
+    public GameObject Enemy8;
+    public GameObject Enemy9;
+    public GameObject Enemy10;
+    public GameObject Enemy11;
+    public GameObject Enemy12;
+    public GameObject Enemy13;
+    public GameObject Enemy14;
+    void Start()
+
+    {
+
+
+        Enemy1.SetActive(false);
+        Enemy2.SetActive(false);
+        Enemy3.SetActive(false);
+        Enemy4.SetActive(false);
+        Enemy5.SetActive(false);
+        Enemy6.SetActive(false);
+        Enemy7.SetActive(false);
+        Enemy8.SetActive(false);
+        Enemy9.SetActive(false);
+        Enemy10.SetActive(false);
+        Enemy11.SetActive(false);
+        Enemy12.SetActive(false);
+        Enemy13.SetActive(false);
+        Enemy14.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+   
+
+    public void EnablingEnemy1() => Enemy1.SetActive(true);
+   
+    public void EnablingEnemy2() => Enemy2.SetActive(true);
+
+    public void EnablingEnemy3() => Enemy3.SetActive(true);
+    
+    public void EnablingEnemy4() =>  Enemy4.SetActive(true);
+    public void EnablingEnemy5() =>  Enemy5.SetActive(true);
+    public void EnablingEnemy6() =>  Enemy6.SetActive(true);
+    public void EnablingEnemy7() =>  Enemy7.SetActive(true);
+    public void EnablingEnemy8() =>  Enemy8.SetActive(true);
+    public void EnablingEnemy9() =>  Enemy9.SetActive(true);
+    public void EnablingEnemy10() =>  Enemy10.SetActive(true);
+    public void EnablingEnemy11() =>  Enemy11.SetActive(true);
+    public void EnablingEnemy12() =>  Enemy12.SetActive(true);
+    public void EnablingEnemy13() =>  Enemy13.SetActive(true);
+    public void EnablingEnemy14() =>  Enemy14.SetActive(true);
+
+}
---
+++ Moon_Patrol_Rework/Assets/Scripts/E-J/ReleasingEnemyE.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleasingEnemyE : MonoBehaviour
+{
+    // Start is called before the first frame update  EnemyInvoke trigger;
+
+    BombInvoke bombTrigger;
+    EnemyInEJ trigger;
+    void Start()
+    {
+
+          trigger = GameObject.Find("Manager").GetComponent<EnemyInEJ>();
+          bombTrigger = GameObject.Find("Manager").GetComponent<BombInvoke>();
+        
+    }
+
+    // Update is called once per frame
+   
+
+   public void OnTriggerEnter2D(Collider2D other)
+  {
+      
+        if (other.gameObject.tag == "Body")
+
+
+        {
+
+            
+            trigger.Invoke("EnablingEnemy1",2f);
+            trigger.Invoke("EnablingEnemy2", 2f);
+            trigger.Invoke("EnablingEnemy3", 9f);
+            trigger.Invoke("EnablingEnemy4", 10f);
+            trigger.Invoke("EnablingEnemy5", 52);
+            trigger.Invoke("EnablingEnemy6", 53);
+            trigger.Invoke("EnablingEnemy7", 30f);
+            trigger.Invoke("EnablingEnemy8", 31f);
+            trigger.Invoke("EnablingEnemy9", 31f);
+            trigger.Invoke("EnablingEnemy10", 38f);
+            trigger.Invoke("EnablingEnemy11", 39f);
+            trigger.Invoke("EnablingEnemy12", 44f);
+            trigger.Invoke("EnablingEnemy13", 45f);
+            trigger.Invoke("EnablingEnemy14", 53f);
+            bombTrigger.Invoke("EnablingBomb1", 3f);
+            bombTrigger.Invoke("EnablingBomb2", 5f);
+            bombTrigger.Invoke("EnablingBomb3", 12f);
+            bombTrigger.Invoke("EnablingBomb4", 25f);
+            bombTrigger.Invoke("EnablingBomb5", 27f);
+            bombTrigger.Invoke("EnablingBomb6", 30f);
+            bombTrigger.Invoke("EnablingBomb7", 50f);
+            bombTrigger.Invoke("EnablingBomb8", 55f);
+            bombTrigger.Invoke("EnablingBomb9", 7f);
+            bombTrigger.Invoke("EnablingBomb10", 7f);
+            bombTrigger.Invoke("EnablingBomb11", 14f);
+            bombTrigger.Invoke("EnablingBomb12", 12f);
+            bombTrigger.Invoke("EnablingBomb13", 14f);
+            bombTrigger.Invoke("EnablingBomb14", 39f);
+            bombTrigger.Invoke("EnablingBomb15", 40f);
+            bombTrigger.Invoke("EnablingBomb16", 42f);
+            bombTrigger.Invoke("EnablingBomb17", 47f);
+            bombTrigger.Invoke("EnablingBomb18", 39f);
+            bombTrigger.Invoke("EnablingBomb19", 22f);
+            bombTrigger.Invoke("EnablingBomb20", 10f);
+            bombTrigger.Invoke("EnablingBomb21", 12f);
+
+
+
+
+        }
+
+      
+
+    }
+}
