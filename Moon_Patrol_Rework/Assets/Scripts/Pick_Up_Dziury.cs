9c5ffa8a1d84033d0a052f91b742243916b18885
Kacperb96 <69037818+Kacperb96@users.noreply.github.com>
2021-01-18 13:13:15 +0100

    uptade

---
+++ Moon_Patrol_Rework/Assets/Scripts/Pick_Up_Dziury.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick_Up_Dziury : MonoBehaviour
+{
+    Counter counter;
+
+    void Start()
+    {
+        counter = GameObject.Find("Manager").GetComponent<Counter>();
+        
+        if (counter == null)
+        {
+
+            Debug.LogError("Cannot find Counter");
+        }
+
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+            counter.IncrementCounter500();
+    }
+}
