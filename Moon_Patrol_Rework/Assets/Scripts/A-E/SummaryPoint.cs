0933486b3a50bd60d5722603aa0997aa3d27effd
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-18 21:28:13 +0100

    s
    s

---
+++ Moon_Patrol_Rework/Assets/Scripts/A-E/SummaryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummaryPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    TimeCounter catcher; 
+    
+    void Start()
+
+    {
+        catcher = GameObject.Find("Manager").GetComponent<TimeCounter>();
+        
+        if (catcher == null)
+        {
+
+            Debug.LogError("Cannot find TimeCounter");
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      
+
+        catcher.ItsFinish();
+
+        Debug.Log("Wykryto");
+
+
+    }
+
+
+}
--- Moon_Patrol_Rework/Assets/Scripts/TimeCounter.cs
+++ Moon_Patrol_Rework/Assets/Scripts/TimeCounter.cs
@@ -2,31 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TimeCounter : MonoBehaviour
 {
-
+    bool lvlFin = false;
+    
     public Text timeCounter;
+    public Text finalTime;
     private float startTime;
+    string seconds;
+    string minutes;
+    private int hiddenmin;
+    private int hiddensec;
+    private int timeSum;
+    string number;
+    int i= 0;
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
         
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float t = Time.time - startTime; 
 
-        string minutes = ((int) t / 60).ToString();
-        string seconds = ((int)t % 60).ToString();
-
-        timeCounter.text = minutes + seconds;
-
-
+        if(lvlFin == false)
+        {
 
         
+            float t = Time.time - startTime; 
+
+            hiddenmin = ((int) t / 60);
+            hiddensec = ((int)t % 60);
+            minutes = ((int) t / 60).ToString();
+            seconds = ((int)t % 60).ToString();
+
+            timeCounter.text = minutes + seconds;
+            
+            timeSum = hiddenmin + hiddensec;
+            
+
+        }
+
+        else
+        {
+            
+            if(i <= timeSum)
+           {
+                number=i.ToString();
+
+                finalTime.text = number; 
+                i++;  
+            
+           }
+
+                
+
+
+
+        }
+
+
     }
+
+    public void ItsFinish()
+    {
+
+        lvlFin = true; 
+    }    
+
+
+
+
+         
+        
+
+
+
+
 }
