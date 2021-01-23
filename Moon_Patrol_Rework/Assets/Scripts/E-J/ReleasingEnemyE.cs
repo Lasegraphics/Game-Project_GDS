1ba8dd7c6ae5ec477ff5916210d9e85b6e7458cd
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-23 21:44:50 +0100

    s
    s

--- Moon_Patrol_Rework/Assets/Scripts/E-J/ReleasingEnemyE.cs
+++ Moon_Patrol_Rework/Assets/Scripts/E-J/ReleasingEnemyE.cs
@@ -30,10 +30,10 @@ public class ReleasingEnemyE : MonoBehaviour
             
             trigger.Invoke("EnablingEnemy1",2f);
             trigger.Invoke("EnablingEnemy2", 2f);
-            trigger.Invoke("EnablingEnemy3", 9f);
-            trigger.Invoke("EnablingEnemy4", 10f);
-            trigger.Invoke("EnablingEnemy5", 52f);
-            trigger.Invoke("EnablingEnemy6", 53f);
+            trigger.Invoke("EnablingEnemy3", 6f);
+            trigger.Invoke("EnablingEnemy4", 7f);
+            trigger.Invoke("EnablingEnemy5", 57f);
+            trigger.Invoke("EnablingEnemy6", 59f);
             trigger.Invoke("EnablingEnemy7", 30f);
             trigger.Invoke("EnablingEnemy8", 31f);
             trigger.Invoke("EnablingEnemy9", 31f);
@@ -41,7 +41,7 @@ public class ReleasingEnemyE : MonoBehaviour
             trigger.Invoke("EnablingEnemy11", 39f);
             trigger.Invoke("EnablingEnemy12", 44f);
             trigger.Invoke("EnablingEnemy13", 45f);
-            trigger.Invoke("EnablingEnemy14", 53f);
+            trigger.Invoke("EnablingEnemy14", 58f);
             bombTrigger.Invoke("EnablingBomb1", 3f);
             bombTrigger.Invoke("EnablingBomb2", 5f);
             bombTrigger.Invoke("EnablingBomb3", 12f);
