bbaa73b357670cf64377a3cbf3cc0796edd0e301
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-24 14:02:13 +0100

    ss
    sd

---
+++ Moon_Patrol_Rework/Assets/Scripts/MainMenu/Choosing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choosing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+     public string scenesName = "A-E";
+
+ 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+           Invoke("LoadLevel",2f);
+
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+
+             Invoke("QuitApp",2f);
+        }
+        
+    }
+
+    void LoadLevel()
+    {
+
+        Application.LoadLevel(scenesName);
+    }
+
+    void QuitApp()
+    {
+         
+         Application.Quit();
+
+
+    }
+
+
+
+}
