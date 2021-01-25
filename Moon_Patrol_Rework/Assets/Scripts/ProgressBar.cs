8800906d62ff415a9fbeb251b18c9f324c043bec
Kacperb96 <69037818+Kacperb96@users.noreply.github.com>
2021-01-25 20:18:39 +0100

    c

---
+++ Moon_Patrol_Rework/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+    [SerializeField] Transform End;
+    [SerializeField] Slider slider;
+
+    float maxDistance;
+
+    void Start()
+    {
+        maxDistance = GetDistance();
+    }
+
+    void Update()
+    {
+        if (Player.position.x <= maxDistance && Player.position.x <= End.position.x)
+        {
+            float distance = 1 - (GetDistance()/maxDistance);
+            setProgress (distance);
+        }
+    }
+
+    float GetDistance()
+    {
+        return Vector2.Distance (Player.position, End.position);
+    }
+
+    void setProgress(float p)
+    {
+        slider.value = p;
+    }
+}
