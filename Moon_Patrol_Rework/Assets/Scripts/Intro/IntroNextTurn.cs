e8eed239fac0a30cc6c698588e25ade3985dd761
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-27 13:58:19 +0100

    sd
    sd

---
+++ Moon_Patrol_Rework/Assets/Scripts/Intro/IntroNextTurn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class IntroNextTurn : MonoBehaviour
+{
+   
+    VideoPlayer m_VideoPlayer;
+    
+    void Awake()
+    {
+        m_VideoPlayer = GetComponent<VideoPlayer>();
+        m_VideoPlayer.loopPointReached += OnMovieFinished;
+    }
+
+    // Update is called once per frame
+   void OnMovieFinished(VideoPlayer player)
+    {
+        Debug.Log("Event for movie end called");
+
+        StartCoroutine(ExecuteAfterTime());
+    }
+
+    IEnumerator ExecuteAfterTime(float time = 3f)
+    {
+        yield return new WaitForSeconds(time);
+
+            Application.LoadLevel(1);
+    }
+
+}
