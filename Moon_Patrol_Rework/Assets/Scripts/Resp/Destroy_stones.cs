edc816fdbde7d468aaca8ff74419f79810b65d2d
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-01-29 18:55:32 +0100

    sd
    sd

--- Moon_Patrol_Rework/Assets/Scripts/Resp/Destroy_stones.cs
+++ Moon_Patrol_Rework/Assets/Scripts/Resp/Destroy_stones.cs
@@ -6,15 +6,32 @@ public class Destroy_stones : MonoBehaviour
 {
 
     public GameObject Anim;
+    Counter counter;
+    void Start()
+    {
 
+        counter = GameObject.Find("Manager").GetComponent<Counter>();
+        
+        if (counter == null)
+        {
+
+            Debug.LogError("Cannot find Counter");
+        }
+
+
+
+    }
     
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Bullet")
         {
             SoundManagerScript.PlaySound("destroy");
+            counter.IncrementCounter100();
             Destroy(gameObject);
             Anim.SetActive(true);
+
+
            
         }
     }
